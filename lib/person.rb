0c8d9943f15a5db3b61431ce74cddc26d353ca8c
Laura Gonzalez <lg458@cornell.edu>
2022-08-12 13:41:39 -0700

    commit tree b702bd5009407ac137603e20e7ee5b656203a8ce

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
+  def name=(name) 
+    @name = name 
+  end 
 
+  def name 
+    @name 
+  end 
+
+  def job=(job) 
+    @job = job
+  end 
+
+  def job 
+    @job 
+  end
 end
+
+kanye = Person.new 
+kanye.name = "Kanye" 
+kanye.job = "pot stirrer"
+
+puts kanye.name
+puts kanye.job
